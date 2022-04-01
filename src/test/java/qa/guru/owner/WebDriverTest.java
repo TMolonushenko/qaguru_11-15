57760f794391123a116383f921b6adab0cd7dc1a
tmolonushenko <tmolonushenko@gmail.com>
2022-04-01 21:44:06 +0300

    Added properties

--- src/test/java/qa/guru/owner/WebDriverTest.java
+++ src/test/java/qa/guru/owner/WebDriverTest.java
@@ -1,5 +1,7 @@
 package qa.guru.owner;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import qa.guru.owner.config.WebDriverProvider;
@@ -8,13 +10,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class WebDriverTest {
-    private WebDriver driver = new WebDriverProvider().get();
+
+private WebDriver driver;
+    @BeforeEach
+    public void startDriver() {
+        driver = new WebDriverProvider().get();
+
+    }
+
 
     @Test
     public void testGitHubTitle() {
         assertEquals(driver.getTitle(), "GitHub: Where the world builds software · GitHub");
+
+    }
+@AfterEach
+    public void stopDriver() {
         driver.quit();
     }
-
 
 }
--- src/test/java/qa/guru/owner/config/WebDriverConfig.java
+++ src/test/java/qa/guru/owner/config/WebDriverConfig.java
@@ -1,23 +1,45 @@
 package qa.guru.owner.config;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Objects;
 
 public class WebDriverConfig {
 
     public String getBaseUrl() {
-        return "https://github.com/";
+        String baseUrl = System.getProperty("baseUrl");//читаем значение
+        if (Objects.isNull(baseUrl)) {//обрабатываем дефолтное
+            baseUrl = "https://github.com";
+        }
+        return baseUrl;// конвертируем результат
     }
 
     public boolean isRemote() {
-        return false;
+        String isRemote = System.getProperty("isRemote"); // читаем значение
+        if (Objects.isNull(isRemote)) { // обрабатываем дефолтное
+            return false;
+        }
+        return Boolean.parseBoolean(isRemote); // конывертируем результат
     }
 
     public Browser getBrowser() {
-        return Browser.FIREFOX;
+        String browser = System.getProperty("browser");
+        if (Objects.isNull(browser)) {
+            browser = Browser.CHROME;
+        }
+        return Browser.valueOf(browser);
     }
 
     public URL getRemoteErl() { //для удаленного браузера, например, selenide
-        return null;
+        String remoteUrl = System.getProperty("remoteUrl");
+        if (Objects.isNull(remoteUrl)) {
+            return null;
+        }
+        try { // конвертируем результат
+            return new URL(remoteUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
